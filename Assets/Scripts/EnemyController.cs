9351869a6306f73e26eddcf52acbc75aa31eb99a
Coursovra <isip_v.r.obuhov@mpt.ru>
2020-11-16 19:10:37 +0300

    0.6
    +50%Skeleton

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform[] PatrolPoints;
+    public NavMeshAgent Agent;
+    public Animator Animation;
+    [SerializeField]
+    private int _currentPatrolPonts;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Agent.SetDestination(PatrolPoints[_currentPatrolPonts].position);
+        if (Agent.remainingDistance <= .2f)
+        {
+            _currentPatrolPonts++;
+            if (_currentPatrolPonts >= PatrolPoints.Length)
+            {
+                _currentPatrolPonts = 0;
+            }
+            Agent.SetDestination(PatrolPoints[_currentPatrolPonts].position);
+        }
+        Animation.SetBool("IsMoving", true);
+    }
+}
+ 
